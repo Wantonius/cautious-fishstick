e99731128f36186895dac8e345323fe914fb307a
Erno Hentonen <erno.hentonen@opiframe.com>
2023-06-20 12:43:39 +0300

    react test

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+
+let Schema = mongoose.Schema({
+	username:{type:String,unique:true},
+	password:String
+})
+
+module.exports = mongoose.model("User",Schema);
